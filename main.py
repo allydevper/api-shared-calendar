969a612b1ce39aa0274d0b5161b5534e653a729e
allydevper <alamaswilmer@gmail.com>
2025-01-22 19:41:38 -0500

    test env

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from typing import Union
-
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    # Acceder a una variable de entorno
+    api_key = os.getenv("SUPABASE_URL")
+    return {"Hello": "World", "API_KEY": api_key}
 
 
 @app.get("/items/{item_id}")
